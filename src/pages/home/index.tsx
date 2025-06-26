c78da7d65b4b4a13197beb277886c8f793d341db
Pedro Henrique Bérgamo <pedroh.bergamo20@gmail.com>
2025-06-26 16:46:41 -0300

    ✨ feat: adicionar componente Hero com animações GSAP e vídeo de fundo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,12 @@
 import { Navbar } from "@/components/navbar";
+import { Hero } from "./components/hero";
 
 export default function Home() {
 	return (
 		<main>
 			<Navbar />
+			<Hero />
+			<div className="bg-black h-dvh" />
 		</main>
 	);
 }
