99e36bc906e0b97d9937d68c4739e2f884f4229b
Pedro Henrique Bérgamo <pedroh.bergamo20@gmail.com>
2025-06-26 12:08:34 -0300

    ✨ feat: integrate Tailwind CSS and update project structure
    - Added Tailwind CSS as a dependency and configured it in vite.config.ts.
    - Removed index.css and replaced it with styles.css for Tailwind integration.
    - Updated Home component to use Tailwind CSS classes for styling.
    - Added favicon link and styles.css reference in index.html.
    - Updated pnpm workspace and lock files to include new dependencies.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import "./index.css";
 import { router } from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
 	return (
-		<main>
-			<h1>Page Home</h1>
+		<main className="flex justify-center items-center bg-gray-100 min-h-svh">
+			<h1 className="font-extrabold text-4xl text-center text-balance tracking-tight scroll-m-20">
+				Page Home
+			</h1>
 		</main>
 	);
 }
